3c6f1a920dcfa2519294ff7ed417ce6b92f961ab
Tzuridis <tzuridis@gmail.com>
2016-09-26 12:28:04 -0400

    Initial Commit

--- public/main.js
+++ public/main.js
@@ -3,24 +3,62 @@ $(document).ready(function () {
     var input = $('input');
     var messages = $('#messages');
 
+    var username = prompt('What is your nickname');
+
     var addMessage = function (message) {
-        messages.append('<div>' + message + '</div>');
+        if (typeof message == 'object') {
+            messages.append('<div>' + message.username + ":" + " " + message.message + '</div>');
+        } else {
+            messages.append('<div>' + username + ":" + " " + message + '</div>');
+        }
     };
 
+    // Show typing
+    // var typing = false;
+    // var timeout = undefined;
+
+    // function timeoutFunction() {
+    //     typing = false;
+    //     socket.emit("isTyping", false);
+    // }
+
+    // input.on('keypress', function (event) {
+    //     if (event.keyCode != 13) {
+    //         if (typing === false) {
+    //             typing = true;
+    //             socket.emit("isTyping", true);
+    //         } else {
+    //             clearTimeout(timeout);
+    //             timeout = setTimeout(timeoutFunction, 50000);
+    //         }
+    //     }
+    //     var message = 'is typing...';
+    //     addMessage(message);
+    //     socket.emit('message', message.message)
+    // });
+
+
+
+    //Enter & show message
     input.on('keydown', function (event) {
         if (event.keyCode != 13) {
             return;
-        }
+        };
 
         var message = input.val();
         addMessage(message);
-        socket.emit('message', message);
+        socket.emit('message', { username: username, message: message });
         input.val('');
     });
 
+
+    //Defining sent
+
+    // socket.on('typing', function () {
+    //     socket.on('isTyping', addMessage);
+    // });
     socket.on('message', addMessage);
-    socket.on('connect', function(){
-  socket.on('users_count', addMessage);
-    console.log("Connection");
-  });
+    socket.on('connect', function () {
+        socket.on('users_count', addMessage);
+    });
 });
--- server.js
+++ server.js
@@ -13,28 +13,29 @@ app.use(express.static('public'));
 var server = http.Server(app);
 var io = socket_io(server);
 
-var users = 0;
-
-var username = "User" + users;
+var usercount = 0;
 
 io.on('connection', function (socket) {
-        users++;
-        console.log('Client connected - Users connected', users);
-        io.emit('users_count', 'Users Connected:' + users);
+        usercount++;
+        console.log('Client connected - Users connected', usercount);
+        io.emit('users_count', 'Users Connected:' + usercount);
 
     socket.on('message', function(message) {
         console.log('Received message:', message);
         socket.broadcast.emit('message', message);
     });
 
-    socket.on('disconnect', function(){
-    users--;
-    console.log('Client disconnected - Users connected', users);
-     io.emit('users_count', 'Users Connected:' + users);
+    socket.on('disconnect', function(message){
+    usercount--;
+    console.log('Client disconnected - Users connected', usercount);
+     io.emit('users_count', 'Users Connected:' + usercount);
   });
+
+//   socket.on('typing', function(message){
+//       io.emit("isTyping", message)
+//   })
 
 });
 
 app.use(express.static('public'));
 server.listen(process.env.PORT || 8080);
-// app.listen(process.env.PORT || 8080);
